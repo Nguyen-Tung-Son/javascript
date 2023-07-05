843fd9f5a0ee98cd2bdd2fae4ce8afc2dd6eb3e7
Nguyen Tung Son <titlai434321@gmail.com>
2023-07-05 09:29:10 +0700

    btvn

---
+++ buoi-10/w3school/JavaScript Modules/script.js
@@ -0,0 +1,12 @@
+const name = "Tung Son";
+const age = 23;
+
+export {name, age};
+
+const info = () => {
+    const name = "Khanh";
+    const age = 20;
+    return name + ' is ' + age + 'years old.';
+    };
+    
+    export default info;
